398a58ed573639b410868b217f84ac5463674722
zkrasnay <zkrasnay@users.noreply.github.com>
2015-08-24 16:13:45 +0200

    Update follows.js

--- follows.js
+++ follows.js
@@ -16,7 +16,7 @@
 		defaultFeeds	= [745057205], // Comma separated array of Xively Feed ID numbers
 		applicationName	= 'Teplota na chalupe', // Replaces Xively logo in the header
 		dataDuration	= '1day', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
-		dataInterval	= 86400, // Default interval for data to be displayed (in seconds)
+		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
 
