d03f6d2157f260e60fd63dea6ddd60c598246ece
zkrasnay <zkrasnay@users.noreply.github.com>
2015-08-24 15:47:34 +0200

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,9 +12,9 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
+	var defaultKey		= 'Dx8zc2O6VZupbaAovo0eZZROgd6h1ojRrmtkNWFrWNxpE5cW', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [745057205], // Comma separated array of Xively Feed ID numbers
+		applicationName	= 'Teplota na chalupe', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
